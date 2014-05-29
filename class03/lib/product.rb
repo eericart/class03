72cc64428f91259bf83f8371a5548f3849eed17b
Ernesto Ricart <ernest2193@gmail.com>
2014-05-29 10:59:58 -0400

    Product #initialize

--- class03/lib/product.rb
+++ class03/lib/product.rb
@@ -7,6 +7,13 @@ class Product
   attr_accessor :title, :price , :category
 
   def initialize (title, price, category)
+    raise TypeError unless title.is_a? String
+    raise TypeError unless price.is_a? Float
+    raise TypeError unless category.is_a? Symbol
+    @title = title
+    @price = price
+    @category = category
+
   end
 
 end
